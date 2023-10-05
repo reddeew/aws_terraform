3caf2ffec8a255790e3e3a41e2acddaca5ac5f39
reddeew <atla.eswarreddy@gmail.com>
2023-10-05 09:01:49 +0530

    assume policy chagnes added

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
 provider "aws" {
   region = "us-east-1"  # Replace with your desired region
 }
+
 resource "aws_vpc" "sandpit" {
   cidr_block = "10.0.0.0/16"
   # Add any other desired parameters
 }
+
 resource "aws_subnet" "sandpit_subnet" {
   vpc_id     = aws_vpc.sandpit.id
   cidr_block = "10.0.0.0/24"
   # Add any other desired parameters
 }
+
 resource "aws_security_group" "sandpit_security_group" {
   vpc_id = aws_vpc.sandpit.id
 
@@ -22,12 +25,32 @@ resource "aws_security_group" "sandpit_security_group" {
 
   # Add any other desired inbound or outbound rules
 }
+
+resource "aws_iam_role" "sandpit_role" {
+  name = "sandpit-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+  # Add any other desired parameters
+}
+
 resource "aws_instance" "sandpit_instance" {
-  ami           = "ami-04cb4ca688797756f"  # Replace with your desired AMI ID
+  ami           = "ami-04cb4ca688797756f"  # Replace with your desired AMI ID in us-east-1
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.sandpit_subnet.id
   vpc_security_group_ids = [aws_security_group.sandpit_security_group.id]
-  iam_instance_profile = aws_iam_role.sandpit_role.name
+  iam_instance_profile = aws_iam_instance_profile.sandpit_instance_profile.name
   # Add any other desired parameters
 }
 
@@ -39,9 +62,4 @@ resource "aws_iam_instance_profile" "sandpit_instance_profile" {
 resource "aws_iam_role_policy_attachment" "sandpit_attachment" {
   role       = aws_iam_role.sandpit_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"  # Replace with your desired policy ARN
-}
-
-resource "aws_iam_role" "sandpit_role" {
-  name = "sandpit-role"
-  # Add any other desired parameters
 }
